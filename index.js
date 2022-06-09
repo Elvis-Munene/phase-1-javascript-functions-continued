eeac1d813dc6001aef8e846f3ed22ce9a564778e
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-10 02:38:25 +0300

    commit tree 2e1d55edc71ae0b7a506b019b3608b5bce1a1c90

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+
+
+//saturdayFun
+
+function saturdayFun(default1 = "roller-skate") {
+    return `This Saturday, I want to ${default1}!`;
+  }
+  console.log(saturdayFun("scga"));
+
+
+//mondayWork
+function mondayWork(default1 = "go to the office") {
+    return `This Monday, I will ${default1}.`;
+  }
+  console.log(mondayWork("fd"));
+
+//wrapAdjective
+function wrapAdjective(praise = "*") {
+    return function (compliment = "special") {
+      return `You are ${praise}${compliment}${praise}!`;
+    };
+  }
+  console.log(wrapAdjective("jh"));
